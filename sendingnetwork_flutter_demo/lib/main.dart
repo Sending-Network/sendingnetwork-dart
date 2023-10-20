c137132a87d4019f65295d2214d7b7c7d4c276dd
cherishwang <cherish@sending.network>
2023-10-20 11:34:07 +0800

    update flutter

--- sendingnetwork_flutter_demo/lib/main.dart
+++ sendingnetwork_flutter_demo/lib/main.dart
@@ -14,11 +14,13 @@ import 'package:intl/intl.dart';
 import 'package:eth_sig_util/eth_sig_util.dart';
 import 'package:path_provider/path_provider.dart';
 
-String privKey = ""; //
-String addressHexAll = "";
-String homeUrl = "https://portal0101.sending.network";
-String developSignUrl = "";
-
+String privKey =
+    "f630d27bafc00df4bfb452586f9e7b4dfda1b3f16e242819311473164a2e28d9"; //
+String addressHexAll = "0xa6dC81DE79ba5BDB908da792d5A96cBB15Cc7424";
+String homeUrl = "https://beginner.sending.network";
+// String homeUrl = "https://portal0101.sending.network";
+// https://portal0101.sending.network
+String developSignUrl = "https://rewards.sending.network/_api/appservice/sign";
 
 late Room currentRoom;
 
