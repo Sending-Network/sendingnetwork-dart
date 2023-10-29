5490b848a5eac6f9d540dbbd007f6cd9cc359c4c
cherishwang <cherish@sending.network>
2023-10-29 16:07:40 +0800

    feat： delete two repo

--- sendingnetworkdart_api_lite/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-
-## 0.1.0 
-Split into independent warehouse
-  
-## 0.0.9
-
-- Initial version
--- sendingnetworkdart_api_lite/example/sdn_api_lite_example.dart
+++
@@ -1,8 +0,0 @@
-// @dart=2.9
-import 'package:sendingnetworkdart_api_lite/src/sdn_api.dart';
-
-void main() async {
-  final api = SDNApi(node: Uri.parse('https://sdn.org'));
-  final capabilities = await api.requestServerCapabilities();
-  print(capabilities.toJson());
-}
